1142ee6952bfffa4639f2f4cc07abe14b420d2f3
Adithya Alokkan <adithya.alokkan@gmail.com>
2025-01-15 12:59:46 +0000

    setup: world

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'maze_runner'
+package_name = "maze_runner"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='adi',
-    maintainer_email='adithya.alokkan@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "worlds"), glob("worlds/*")),
+        *[
+            (os.path.join("share", package_name, os.path.dirname(f)), [f])
+            for f in glob("models/**", recursive=True)
+            if os.path.isfile(f)
         ],
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="adi",
+    maintainer_email="adithya.alokkan@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
     },
 )
