a452604c1040dbb4cdaa0851149cc7ad0c67e59d
Adithya Alokkan <adithya.alokkan@gmail.com>
2025-01-15 14:28:29 +0000

    feat: create mapping launch file

---
+++ launch/mapping.launch.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    turtlebot3_launch_dir = os.path.join(
+        get_package_share_directory("turtlebot3_gazebo"), "launch"
+    )
+    world_file = os.path.join(
+        get_package_share_directory("autonomous_tb3"),
+        "worlds",
+        "maze_5x5.world",
+    )
+    config_dir = os.path.join(get_package_share_directory("autonomous_tb3"), "config")
+    rviz_config = os.path.join(config_dir, "tb3_nav.rviz")
+    pkg_gazebo_ros = get_package_share_directory("gazebo_ros")
+
+    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
+    x_pose = LaunchConfiguration("x_pose", default="0.0")
+    y_pose = LaunchConfiguration("y_pose", default="0.0")
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, "launch", "gzserver.launch.py")
+        ),
+        launch_arguments={"world": world_file}.items(),
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, "launch", "gzclient.launch.py")
+        )
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_launch_dir, "robot_state_publisher.launch.py")
+        ),
+        launch_arguments={"use_sim_time": use_sim_time}.items(),
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_launch_dir, "spawn_turtlebot3.launch.py")
+        ),
+        launch_arguments={"x_pose": x_pose, "y_pose": y_pose}.items(),
+    )
+
+    maze_mapping = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("slam_toolbox"),
+                "launch",
+                "online_async_launch.py",
+            )
+        ),
+    )
+
+    rviz = Node(
+        package="rviz2",
+        output="screen",
+        executable="rviz2",
+        name="rviz2_node",
+        arguments=["-d", rviz_config],
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
+
+    ld.add_action(rviz)
+    ld.add_action(maze_mapping)
+
+    return ld
