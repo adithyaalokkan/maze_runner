e7c5e7ee7908306cc2a2a4e7487763559eb747ca
Adithya Alokkan <adithya.alokkan@gmail.com>
2025-01-15 14:25:53 +0000

    setup: create config files

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
         (os.path.join("share", package_name, "worlds"), glob("worlds/*")),
         *[
             (os.path.join("share", package_name, os.path.dirname(f)), [f])
